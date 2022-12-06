624be9b5f3a485fb9af834614440ec70b4c0938e
uslan <bublikrs5@mail.ru>
2022-12-06 19:51:50 +0300

    first commit

---
+++ Task01/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.Write("Введите любое трехзначное число: ");
+int digit = Convert.ToInt32(Console.ReadLine());
+
+int digit1 = digit/10;
+int digit2 = digit1%10;
+Console.WriteLine(digit2);
+ 
+ 
+
---
+++ Task2/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+while(number > 999)
+    {
+    number=number/10;
+    }
+Console.WriteLine($"{number%10}");
